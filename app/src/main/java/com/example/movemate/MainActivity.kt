f7fbe7dd7450da0afdf7dd9644e7737a9bfb46d8
HoverKK <jakub.konicki@onet.pl>
2023-11-21 00:11:06 +0100

    modyfikacje z kaloriamii

--- app/src/main/java/com/example/movemate/MainActivity.kt
+++ app/src/main/java/com/example/movemate/MainActivity.kt
@@ -7,10 +7,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
@@ -18,11 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_historia -> {
-                showToast("Historia clicked")
-                return true
-            }
-
             R.id.action_kalorie -> {
                 showToast("Kalorie clicked")
                 return true
