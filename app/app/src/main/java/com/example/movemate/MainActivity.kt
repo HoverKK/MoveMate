b0a36e27944348b422f430e239eb7872f8af34bc
HoverKK <jakub.konicki@onet.pl>
2023-11-26 09:44:32 +0100

    kalorie

--- app/app/src/main/java/com/example/movemate/MainActivity.kt
+++ app/app/src/main/java/com/example/movemate/MainActivity.kt
@@ -11,9 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView: ListView = findViewById(R.id.listView)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, foodItemList.map { it.name })
-        listView.adapter = adapter
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -44,22 +41,4 @@ class MainActivity : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-    private fun calculateTotalCalories() {
-        val totalCalories = foodItemList.sumBy { it.calories }
-        Toast.makeText(this, "Total Calories: $totalCalories", Toast.LENGTH_SHORT).show()
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_calculate_calories -> {
-                calculateTotalCalories()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-    private val foodItemList = mutableListOf(
-        FoodItem("Apple", 95),
-        FoodItem("Banana", 105),
-        FoodItem("Yogurt", 150)
-    )
 }
---
+++ app/src/main/java/com/example/movemate/FoodItem.kt
@@ -0,0 +1,4 @@
+package com.example.movemate
+
+class FoodItem { data class FoodItem(val name: String, val calories: Int)
+}
