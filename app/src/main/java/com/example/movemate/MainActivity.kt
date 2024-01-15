3517f637255f8569b0d5c9b6fe7e13162cd5bc1f
MaxenWolf <radek.maks.b@gmail.com>
2024-01-15 22:07:48 +0100

    dalszy ciąg krokomierza

--- app/src/main/java/com/example/movemate/MainActivity.kt
+++ app/src/main/java/com/example/movemate/MainActivity.kt
@@ -293,7 +293,70 @@ class MainActivity<ListView> : AppCompatActivity() {
             stepSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         }
 
+        protected void onResume(){
+            super.onResume();
 
+            if (stepSensor == null){
+                Toast.makeText(this, "This device has no sensor", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                mSensorManager.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+
+        protected void onPause(){
+            super.onPause();
+            mSensorManager.unregisterListener(this);
+        }
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+                totalSteps = (int) event.values[0];
+                int currentSteps = totalSteps-previewsTotalSteps;
+                steps.setText(String.valueOf(currentSteps));
+
+                progressBar.setProgress(currentSteps);
+            }
+        }
+
+        private void resetSteps(){
+            steps.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Toast.makeText(MainActivity.this, "Long press to reset steps", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            steps.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v){
+                    previewsTotalSteps = totalSteps;
+                    steps.setText("0");
+                    progressBar.setProgress(0);
+                    saveDate();
+                    return true;
+                }
+            });
+        }
+
+        private void saveDate(){
+            SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("key1",String.valueOf(previewsTotalSteps));
+            editor.apply();
+
+        }
+
+        private void loadDate(){
+            SharedPreferences sharedPref = getSharedPreferences("myPref",Context.MODE_PRIVATE);
+            int savedNumber = (int) sharedPref.getFloat("key1", 0f);
+            previewsTotalSteps = savedNumber;
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
 
 
 
